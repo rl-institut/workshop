d826c446c8dfb50fd6ad2290e83f78f86d30d03a
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2022-02-09 13:09:06 +0100

    Add code and more tests

--- test-driven-development/code_def/calc.py
+++ test-driven-development/code_def/calc.py
@@ -1,2 +1,7 @@
+#this one allows checking each argument's type
 class Calc:
-    pass
+    def add(self, *args):
+        answer = 0
+        for num in args:
+            answer = answer + num
+        return answer
--- test-driven-development/tests/test_calc.py
+++ test-driven-development/tests/test_calc.py
@@ -1,16 +1,34 @@
 import pytest
 
-from code_def.calc import Calc
+from code_def.calc import Calculator
 
 # run these tests with `pytest tests/test_calc.py` or `py.test tests/test_calc.py`
 
+@pytest.fixture
+def c():
+    return Calculator()
 
-def test_add_two_numbers():
-    c = Calc()
+
+def test_add_two_numbers(c):
 
     res = c.add(4, 5)
 
     assert res == 9
+
+    
+def test_add_three_numbers(c):
+
+    res = c.add(4, 5, 6)
+
+    assert res == 15, "some helpful message"
+  
+
+from code_def.calc import Calc
+
+def test_add_many_numbers():
+    s = range(100)
+
+    assert Calc().add(*s) == 4950 
 
 # Exercise A
 # add a method to the class Calc with the following requirements :
