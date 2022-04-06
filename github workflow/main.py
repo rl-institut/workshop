72a11d114d642082858954d93770f5aaf822f780
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2022-04-06 10:48:13 +0200

    Add main dummy code

---
+++ github workflow/main.py
@@ -0,0 +1,7 @@
+from src.cakes import banana_cake
+from src.sauces import vanilla_sauce
+
+my_banana_cake = banana_cake("flour", "banana", "sugar", "milk")
+a_vanilla_sauce = vanilla_sauce("eggs", "milk", "vanilla", "sugar")
+
+print(a_vanilla_sauce)
